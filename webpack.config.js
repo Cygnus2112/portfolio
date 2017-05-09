aeedae076b9c6cb0a3966a1e2a67bd83c58ec2b7
Thomas Leupp <tomleupp@gmail.com>
2017-05-08 17:53:22 -0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const webpack = require('webpack');
+const packageJson = require('./package.json');
+
+module.exports = {
+    entry: {
+        app: './source/index.js',
+        vendor: Object.keys(packageJson['dependencies'])
+    },
+    output: {
+        path: 'build',
+        filename: '[name].js'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['', '.js', '.webpack.js', '.web.js']
+    },
+    module: {
+        loaders: [
+            { loader: 'json-loader', test: /\.json$/ },
+            { loader: 'babel-loader', test: /\.js$/, exclude: /node_modules/, query: { presets: ['react', 'es2015']}}
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')
+    ]
+};

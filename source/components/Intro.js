9955eb1967a23f2925ca1d4ac9ceaf6662aed43e
Thomas Leupp <tomleupp@gmail.com>
2017-05-16 10:25:42 -0700

    tweaked links

--- source/components/Intro.js
+++ source/components/Intro.js
@@ -12,8 +12,8 @@ export class Intro extends React.Component {
 
         <h2 id='occupation'>Full-Stack Web & Mobile Developer</h2>
         <div className='icons'>
-          <a href='https://github.com/Cygnus2112'><img src='/assets/github.png' alt='GitHub' width='50px' height='50px' /></a>
-          <a href='https://www.linkedin.com/in/thomasleupp'><img src='/assets/linkedin.png' alt='linkedin' width='50px' height='50px' /></a>
+          <a href='https://github.com/Cygnus2112' target="_blank"><img src='/assets/github.png' alt='GitHub' width='50px' height='50px' /></a>
+          <a href='https://www.linkedin.com/in/thomasleupp' target="_blank"><img src='/assets/linkedin.png' alt='linkedin' width='50px' height='50px' /></a>
         </div>
       </div>
     </div>
--- source/components/Projects.js
+++ source/components/Projects.js
@@ -13,14 +13,14 @@ export class Projects extends React.Component {
     
   			<ul>
     			<li>
-            <a href='https://play.google.com/store/apps/details?id=com.fencer'>
+            <a href='https://play.google.com/store/apps/details?id=com.fencer' target="_blank">
       				<span className='project-image'><img src='/assets/fencer.jpg' alt='Fencer' width='355px' height='200px' /></span>
             </a>
             <p className='project-title'>Fencer</p>
             <div className='tech-stack'><span className='left-diamond'>&#9670; </span> <span style={{'paddingTop': '1px', 'fontStyle': 'italic'}}>React Native, Redux, Node.js, Express.js, MongoDB</span> <span className='right-diamond'> &#9670;</span></div>
     			</li>
     			<li>
-            <a href='http://budgieapp.herokuapp.com/'>
+            <a href='http://budgieapp.herokuapp.com/' target="_blank">
       				<span className='project-image'><img src='/assets/budgie.png' alt='Budgie' width='355px' height='200px' /></span>
             </a>
             <p className='project-title'>Budgie</p>
@@ -29,21 +29,21 @@ export class Projects extends React.Component {
         </ul>
         <ul>
         	<li>
-            <a href='https://play.google.com/store/apps/details?id=com.beermeandroid'>
+            <a href='https://play.google.com/store/apps/details?id=com.beermeandroid' target="_blank">
       				<span className='project-image'><img src='/assets/beerme.png' alt='BeerMe!' width='355px' height='200px' /></span>
             </a>
             <p className='project-title'>BeerMe!</p>
             <div className='tech-stack'><span className='left-diamond'>&#9670; </span> <span style={{'paddingTop': '1px', 'fontStyle': 'italic'}}>React Native, Redux, Node.js, Express.js, MongoDB</span> <span className='right-diamond'> &#9670;</span></div>
     			</li>
         	<li>
-            <a href='http://gameswap-app.herokuapp.com'>
+            <a href='http://gameswap-app.herokuapp.com' target="_blank">
       				<span className='project-image'><img src='/assets/gameswap.png' alt='GameSwap' width='355px' height='200px' /></span>
             </a>
             <p className='project-title'>GameSwap</p>
             <div className='tech-stack'><span className='left-diamond'>&#9670; </span> <span style={{'paddingTop': '1px', 'fontStyle': 'italic'}}>AngularJS, Bootstrap, HTML5, CSS3, MySQL</span> <span className='right-diamond'> &#9670;</span></div>
     			</li>
   			</ul>
-        <footer id='foot'>Copyright 2017 Thomas Leupp</footer>
+        <footer id='foot'>Copyright Thomas Leupp 2017</footer>
 		</div>
 	}
 }

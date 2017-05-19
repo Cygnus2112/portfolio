40efcd3bf94009829b8a494cbcbeb7d5c74c37c8
Thomas Leupp <tomleupp@gmail.com>
2017-05-18 17:31:12 -0700

    blog tweaks

--- source/components/Blog.js
+++ source/components/Blog.js
@@ -22,30 +22,39 @@ export class Blog extends React.Component {
           <p className='section-title'>componentDidMount</p>
           
           <p>componentDidMount() is called immediately after a component is mounted. If you{"'"}re fetching data from a third-party API or setting up event listeners, this is the place to instantiate those operations. </p>
-          <p>React Native exposes several APIs that allow you to hook into device features, including AppState, BackHandler, and Geolocation, all of which use the familiar event listener callback syntax. For Fencer, an Android app I built that enables users to create and share Snapchat-style Geofilters, I need to track a user{"'"}s location to determine if they{"'"}ve entered a geofence area. I utilized componentDidMount() to set up a watcher using React Native{"'"}s Geolocation API:</p>
-       
+          <p>React Native exposes several APIs that allow you to hook into device features, including AppState, BackHandler, and Geolocation, all of which use JavaScript{"'"}s familiar event listener syntax. For Fencer, an Android app I built that enables users to create and share Snapchat-style Geofilters, I need to track a user{"'"}s location to determine if they{"'"}ve entered a geofence area. I utilized componentDidMount() to set up a watcher using React Native{"'"}s Geolocation API:</p>
+          <p className='code'>
+            <code>
+              {"this.locationWatcher = null; componentDidMount(){ navigator.geolocation.watchPosition((pos) => { this.setState({latitude: pos.coords.latitude,longitude: pos.coords.longitude})}}"}
+            </code>
+          </p>
+          <p>Whenever you call setState() within componentDidMount(), it will trigger a re-render.</p>
           <p className='section-title'>componentWillReceiveProps</p>
-          <p>componentWillReceiveProps() is invoked before a mounted component receives new props.</p> 
-          <p>
-            <code>{"class Greeting extends React.Component {"}
-                  {" render() {"}
-                  {"   return <h1>Hello, {this.props.name}</h1>;"}
-                  {"}"}
-                  {"}"}
+          <p>The first lifecycle invoked in the Updating phase is componentWillReceiveProps(). React is kind enough to give our components a little heads up to let them know whenever new props are on the way. We can use componentWillReceiveProps() whenever we want to update the state in response to prop changes.</p>
+          <p>Fencer uses AirBnb{"'"}s React Native Maps for its geofencing interface and Google Places Autocomplete to allow Geofilter creators to choose specific landmarks for their geofences. When a user chooses a new location using Google Places Autocomplete, the location info is passed down to a Map component, which re-centers according to the new coordinates.</p> 
+          <p className='code'>
+            <code>{"componentWillReceiveProps(nextProps){if(nextProps.chosenLocation !== this.props.chosenLocation) {this.reCenterMap(nextProps.chosenLocation);}}"}
             </code>
           </p> 
-          <p>If you need to update the state in response to prop changes (for example, to reset it), you may compare this.props and nextProps and perform state transitions using this.setState() in this method.</p> 
-          <p>Note that React may call this method even if the props have not changed, so make sure to compare the current and next values if you only want to handle changes. This may occur when the parent component causes your component to re-render.React doesnt call componentWillReceiveProps with initial props during mounting. It only calls this method if some of components props may update. Calling this.setState generally doesnt trigger componentWillReceiveProps.</p>
+          <p>You{"'"}ll notice I added a conditional if statement to ensure chosenLocation only updates if the incoming data is different. This is because React sometimes calls componentWillReceiveProps() even if the props have not changed. </p> 
+          <p>As with componentDidMount(), calling setState() within this method will trigger a re-render. It is not called during mounting.</p>
           <p className='section-title'>shouldComponentUpdate</p>
-          <p>I probably should use this one, but I dont.</p>
+          <p>Resources can be pretty scarce on a mobile device, so it{"'"}s important to utilize every performance optimization at your disposal. React Native{"'"}s usual protocol is to call render() whenever props or state change. As you can imagine, this can get rather expensive if we{"'"}re dealing with lots of components. Thankfully we have the lifecycle method shouldComponentUpdate(), which enables us to short-circuit the render() call if a state change or incoming props are irrelevant to a component{"'"}s UI. The method takes two parameters, nextProps and nextState, and returns a boolean value, which defaults to true.</p>
+          <p>shouldComponentUpdate() is invoked before rendering when new props or state are being received. If it returns false, the remaining two lifecycle methods in the Updating phase, componentWillUpdate() and componentDidUpdate(), will not be called. You cannot call setState() inside this method.</p>
           <p className='section-title'>componentWillUpdate</p>
-          <p>Never use this one.</p>
-
+          <p>componentWillUpdate() is invoked immediately before rendering when new props or state are being received. Like shouldComponentUpdate(), it takes nextProps and nextState as parameters. You cannot call setState() inside this method.</p>
+          <p>Though I{"'"}m certain a number of good use cases exists for this method, I must admit that I have yet to encounter one on any of the React Native apps I{"'"}ve worked on thus far.</p>
           <p className='section-title'>componentDidUpdate</p>
 
-          <p>Never use this one either.</p>
+          <p>componentDidUpdate() is called immediately after a component has updated. It takes two parameters, prevProps and prevState. It can be used to update UI elements or initiate network requests in response to state or props changes. You can call setState within this method.</p>
           <p className='section-title'>componentWillUnmount</p>
-          <p>I do use this one, but I dont feel like talking about it.</p>
+          <p>The final lifecycle method, componentWillUnmount(), is invoked immediately before a component is unmounted and destroyed. This is the place to cancel event listeners, debounce any setTimeouts or setIntervals, and do any other cleanup to avoid memory leaks.</p>
+          <p>
+            <code>
+              {"componentWillUnmount(){navigator.geolocation.clearWatch(locationWatcher);}"}
+            </code>
+          </p>
+
         </div>
       </div>
     </div>
